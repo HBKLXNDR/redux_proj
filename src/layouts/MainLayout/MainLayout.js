e2d75bdcb9efa4bceeef4454dc4967fa89420ea3
markkravic <hbklksndr@protonmail.com>
2022-05-17 16:06:22 +0300

    US,UE

--- src/layouts/MainLayout/MainLayout.js
+++ src/layouts/MainLayout/MainLayout.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import css from "./MainLayout.module.css";
+import {axiosService} from "../../services";
+import {productService} from "../../services/product.service";
 
 const MainLayout = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(()=>{
+        productService.getAllL().then((data)=> console.log(data))
+    },[])
+    const openModal = (id) => {
+        console.log(id);
+        return (<div>
+            ${id}
+        </div>)
+    }
     return (
         <div className={css.main_wrap}>
             <div className={css.item_card}>
@@ -12,7 +24,7 @@ const MainLayout = () => {
                 <p>weight:{"weight"}</p>
                 <div className={css.buttons}>
                     <button>add</button>
-                    <button>delete</button>
+                    <button onClick={()=>openModal()}>delete</button>
                 </div>
 
             </div>
